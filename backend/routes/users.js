b5c7f93706053dfb760429bf8df2afbacefa870c
harshil-mistry <harshilmistry31@gmail.com>
2025-08-14 22:53:12 +0530

    Models moved to backend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,26 +4,32 @@ var router = express.Router();
 const { body, validationResult } = require('express-validator');
 
 /* POST users listing. */
-router.post('/',[
+router.post('/', [
+
   //This middleware is for checking for proper parameters are being passde ot not
   body('name', 'Please enter a proper name').notEmpty(),
   body('email', 'Please enter a valid email').isEmail(),
   body('password', 'The Password should have minimum 8 characters').isLength({
-    min:8
+    min: 8
   })
-], function(req, res, next) {
+
+], function (req, res, next) {
+
   const errors = validationResult(req)
+
   console.log(req.body)
-  if(!errors.isEmpty()){
-    return res.json({
-      "status":"failed",
-      "errors":errors.array()
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      "status": "failed",
+      "errors": errors.array()
     })
   }
+
   res.json({
-    status:"success",
-    message:"Data is valid"
+    status: "success",
+    message: "Successfully signed up"
   });
+
 });
 
 module.exports = router;
