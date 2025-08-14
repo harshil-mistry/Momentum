2dd45f41b95ddcc1a88c7dc7eeabd7abab4fc653
harshil-mistry <harshilmistry31@gmail.com>
2025-08-14 21:34:03 +0530

    Express validator added

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const { body, validationResult } = require('express-validator');
+
+/* POST users listing. */
+router.post('/',[
+  //This middleware is for checking for proper parameters are being passde ot not
+  body('name', 'Please enter a proper name').notEmpty(),
+  body('email', 'Please enter a valid email').isEmail(),
+  body('password', 'The Password should have minimum 8 characters').isLength({
+    min:8
+  })
+], function(req, res, next) {
+  const errors = validationResult(req)
+  console.log(req.body)
+  if(!errors.isEmpty()){
+    return res.json({
+      "status":"failed",
+      "errors":errors.array()
+    })
+  }
+  res.json({
+    status:"success",
+    message:"Data is valid"
+  });
 });
 
 module.exports = router;
---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const { type } = require('os')
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type:String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+const user = new mongoose.model('user', userSchema)
+
+module.exports = user
