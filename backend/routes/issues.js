87ebae6dd149019749fe1733afda3642ab0cd8ca
harshil-mistry <harshilmistry31@gmail.com>
2025-08-24 01:48:11 +0530

    Change issue status doe

--- backend/routes/issues.js
+++ backend/routes/issues.js
@@ -23,48 +23,48 @@ router.post('/:project_id', [auth, [
     try {
         //Check whether project exists or not
         const project_id = req.params.project_id
-            const project_data = await project.findById(project_id)
-            if (!project_data) {
-                return res.status(404).json({
-                    "status":"failed",
-                    "errors":[{
-                        "msg":"Project not found"
-                    }]
-                })
-            }
-
-        //Check whether user owns the project or not
-        if (project_data.owner != req.user) {
-            return res.status(401).json({
-                "status":"failed",
-                "errors":[{
-                    "msg":"You are not authorized for this project"
+        const project_data = await project.findById(project_id)
+        if (!project_data) {
+            return res.status(404).json({
+                "status": "failed",
+                "errors": [{
+                    "msg": "Project not found"
                 }]
             })
         }
 
-        const {name, description} = req.body
+        //Check whether user owns the project or not
+        if (project_data.owner != req.user) {
+            return res.status(401).json({
+                "status": "failed",
+                "errors": [{
+                    "msg": "You are not authorized for this project"
+                }]
+            })
+        }
+
+        const { name, description } = req.body
 
         //Add issue
         const Issue_data = issue({
-            name, 
+            name,
             project: project_id
         })
         if (description) Issue_data.description = description
-        
+
         await Issue_data.save()
         return res.json({
-            "status":"success",
-            "message":"Issue added successfully"
+            "status": "success",
+            "message": "Issue added successfully"
         })
 
 
     } catch (error) {
         console.log(error)
         return res.status(500).json({
-            "status":"failed",
-            "errors":[{
-                "msg":"Internal Server Error"
+            "status": "failed",
+            "errors": [{
+                "msg": "Internal Server Error"
             }]
         })
     }
@@ -75,13 +75,13 @@ router.post('/:project_id', [auth, [
 router.put('/:id', [auth, [
     body('name', 'Please enter a name').notEmpty(),
     body('status')
-    .if(body('status').exists())
+        .if(body('status').exists())
         .isInt().withMessage('Please enter an Integer Value')
-    .isIn([0, 1, 2]).withMessage("The status must be from 0, 1 and 2"),
+        .isIn([0, 1, 2]).withMessage("The status must be from 0, 1 and 2"),
     body('priority')
-    .if(body('priority').exists())
-    .isInt().withMessage('Please enter an Integer Value')
-    .isIn([0, 1, 2]).withMessage("The priority must be from 0, 1 and 2")
+        .if(body('priority').exists())
+        .isInt().withMessage('Please enter an Integer Value')
+        .isIn([0, 1, 2]).withMessage("The priority must be from 0, 1 and 2")
 ]], async (req, res) => {
 
     //Performing data validation
@@ -100,43 +100,43 @@ router.put('/:id', [auth, [
         const issue_data = await issue.findById(issue_id).populate('project')
         if (req.user != issue_data.project.owner) {
             return res.status(401).json({
-                "status":"failed",
-                "errors":[{
-                    "msg":"Unauthorized access"
+                "status": "failed",
+                "errors": [{
+                    "msg": "Unauthorized access"
                 }]
             })
         }
 
         //updating Issue
-        const {name, description, status, priority} = req.body
+        const { name, description, status, priority } = req.body
         const new_issue = {
             name
         }
-        new_issue.description = description? description : ""
+        new_issue.description = description ? description : ""
         if (status) new_issue.status = status
         if (priority) new_issue.priority = priority
-        
+
         const updated = await issue.findByIdAndUpdate(issue_id, new_issue)
-        if(!updated) {
+        if (!updated) {
             return res.status(400).json({
-                "status":"failed",
-                "errors":[{
-                    "msg":"Failed to update the issue"
+                "status": "failed",
+                "errors": [{
+                    "msg": "Failed to update the issue"
                 }]
             })
         }
 
         res.json({
-            "status":"success",
-            "message":"Issue updated successfully"
+            "status": "success",
+            "message": "Issue updated successfully"
         })
 
     } catch (error) {
         console.log(error)
         return res.status(500).json({
-            "status":"failed",
-            "errors":[{
-                "msg":"Internal Server Error"
+            "status": "failed",
+            "errors": [{
+                "msg": "Internal Server Error"
             }]
         })
     }
@@ -154,32 +154,33 @@ router.delete('/:id', auth, async (req, res) => {
         if (req.user != issue_data.project.owner) {
             console.log(req.user, issue_data.owner)
             return res.status(401).json({
-                "status":"failed",
-                "errors":[{
-                    "msg":"Unauthorized access"
+                "status": "failed",
+                "errors": [{
+                    "msg": "Unauthorized access"
                 }]
             })
         }
 
-       await issue.findByIdAndDelete(issue_id)
+        await issue.findByIdAndDelete(issue_id)
 
         res.json({
-            "status":"success",
-            "message":"Issue deleted successfully"
+            "status": "success",
+            "message": "Issue deleted successfully"
         })
 
     } catch (error) {
         console.log(error)
         return res.status(500).json({
-            "status":"failed",
-            "errors":[{
-                "msg":"Internal Server Error"
+            "status": "failed",
+            "errors": [{
+                "msg": "Internal Server Error"
             }]
         })
     }
 })
 
-router.get('/:project_id', auth, async(req, res) => {
+//Get all the issues for a particular project
+router.get('/:project_id', auth, async (req, res) => {
 
     try {
 
@@ -188,14 +189,14 @@ router.get('/:project_id', auth, async(req, res) => {
         const project_data = await project.findById(project_id)
         if (project_data.owner != req.user) {
             res.status(401).json({
-                "status":"failed",
-                "errors":[{
-                    "msg":"Unauthorized access"
+                "status": "failed",
+                "errors": [{
+                    "msg": "Unauthorized access"
                 }]
             })
         }
         console.log(project_id)
-        const issues = await issue.find({project:new mongoose.Types.ObjectId(project_id)})
+        const issues = await issue.find({ project: new mongoose.Types.ObjectId(project_id) })
         console.log(issues)
         res.json({
             "issues": issues
@@ -203,14 +204,63 @@ router.get('/:project_id', auth, async(req, res) => {
     } catch (error) {
         console.log(error)
         res.status(500).json({
-            "status":"failed",
-            "errors":[{
-                "msg":"Internal Server Error"
+            "status": "failed",
+            "errors": [{
+                "msg": "Internal Server Error"
             }]
         })
     }
 })
 
-//TO-DO : Changing the state of the issue (0 <-> 1 <-> 2)
+//Change the status of the issue
+router.patch('/:id', [auth, [
+    body('status')
+        .notEmpty().withMessage("A status is required")
+        .isInt().withMessage("The status has to be an integer")
+        .isIn([0, 1, 2]).withMessage("Status need to be from 0, 1 and 2")
+]], async (req, res) => {
+
+    //Performing data validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            "status": "failed",
+            "errors": errors.array()
+        })
+    }
+
+    try {
+
+        //Checking for authorized access
+        const issue_id = req.params.id
+        const issue_data = await issue.findById(issue_id).populate('project')
+        if (req.user != issue_data.project.owner) {
+            return res.status(401).json({
+                "status": "failed",
+                "errors": [{
+                    "msg": "Unauthorized access"
+                }]
+            })
+        }
+        const newstatus = req.body.status
+        issue_data.status = newstatus
+        await issue_data.save()
+
+        res.json({
+            "status":"success",
+            "message":"Issue status changed successfully"
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            "status": "failed",
+            "errors": [{
+                "msg": "Internal Server Error"
+            }]
+        })
+
+    }
+})
 
 module.exports = router
