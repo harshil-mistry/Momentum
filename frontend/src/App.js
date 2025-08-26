5550d9d057f44993f9b0a78a2c187e61acf6e680
harshil-mistry <harshilmistry31@gmail.com>
2025-08-26 23:17:07 +0530

    Demo frontend added

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Demo from './components/demo';
+
+function App() {
+  return (
+    <Demo/>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/demo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const demo = () => {
+    return(
+        <>
+            <h1>This is demo heading</h1>
+            <p>This is demo para</p>
+        </>
+    )
+}
+
+export default demo
